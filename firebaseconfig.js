8388b6bb514fa2ac8a7ee2846d54a5e1a2dd8d02
monarchofvoid <sinabhinav19@gmail.com>
2025-09-09 11:50:57 +0530

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -5,15 +5,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBK0qK61tnH89ITMtcx8WqeB3Fodl1Q4rY",
-  authDomain: "trash-map-2025.firebaseapp.com",
-  projectId: "trash-map-2025",
-  storageBucket: "trash-map-2025.firebasestorage.app",
-  messagingSenderId: "748873818848",
-  appId: "1:748873818848:web:405bef264f514a610da685",
-  measurementId: "G-PP68VF3PKN"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -22,3 +14,4 @@ const analytics = getAnalytics(app);
 // Export auth & db so register.js can use them
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
